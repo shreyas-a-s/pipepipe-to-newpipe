feeafb6220b1848cbfa0de2942ca29d659618316
Shreyas A S <137637016+shreyas-a-s@users.noreply.github.com>
2023-11-23 20:08:15 +0530

    Create pipepipe-to-newpipe.sh

---
+++ pipepipe-to-newpipe.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+# Install dependencies
+if which apt-get > /dev/null; then
+  sudo apt-get install -y zip unzip sqlite3
+fi
+
+# Open the zip file and extract contents
+unzip PipePipe.zip
+
+# Connect to the SQLite database
+sqlite3 newpipe.db <<EOF
+
+-- Rename the column display_index TO is_thumbnail_permanent
+ALTER TABLE playlists RENAME COLUMN display_index TO is_thumbnail_permanent;
+
+-- Update all values in the renamed column to 0
+UPDATE playlists SET is_thumbnail_permanent = 0;
+
+-- Delete the column display_index
+ALTER TABLE remote_playlists DROP COLUMN display_index;
+
+EOF
+
+# Create a zip file
+zip -r NewPipe.zip newpipe.db newpipe.settings
+
+# Remove extracted files
+rm newpipe.db newpipe.settings
