4a45fbcf1832da760af80ee2d65858008aad3933
Shreyas A S <137637016+shreyas-a-s@users.noreply.github.com>
2025-03-07 00:00:51 +0530

    feat(flutter): Add file_picker package

--- pipe2new/macos/Flutter/GeneratedPluginRegistrant.swift
+++ pipe2new/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,8 +5,10 @@
 import FlutterMacOS
 import Foundation
 
+import file_picker
 import flutter_archive
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  FilePickerPlugin.register(with: registry.registrar(forPlugin: "FilePickerPlugin"))
   FlutterArchivePlugin.register(with: registry.registrar(forPlugin: "FlutterArchivePlugin"))
 }
