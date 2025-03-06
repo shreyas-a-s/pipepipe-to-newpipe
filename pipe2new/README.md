678e87572834fab8f6b512f997a9302eb5a54884
Shreyas A S <137637016+shreyas-a-s@users.noreply.github.com>
2025-03-06 22:30:23 +0530

    feat(flutter): Start creating flutter app

---
+++ pipe2new/README.md
@@ -0,0 +1,3 @@
+# pipe2new
+
+A new flutter app to convert pipepipe database to be imported to newpipe.
---
+++ pipe2new/android/app/src/main/kotlin/com/example/pipe2new/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.pipe2new
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
